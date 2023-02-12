1263528a0152d47a17acb8db353b820c3203e3f0
Israa <48530021+israaBR@users.noreply.github.com>
2023-02-13 00:48:19 +0200

    update movie card design

--- components/movie.jsx
+++ components/movie.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, Button, Image, Text, View } from "react-native";
 
 export const Movie = (props) => {
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
@@ -18,17 +18,12 @@ export const Movie = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "space-around",
-    width: 120,
-    height: "auto",
-    paddingHorizontal: "5%",
+    width: "30%",
     paddingVertical: "5%",
   },
   image: {
     flex: 1,
   },
-
   title: {
     flex: 1,
     fontSize: 16,
